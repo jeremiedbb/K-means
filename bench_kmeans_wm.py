fbf6ca0d794d35bf6ac34e9c86e20e4e1fd2f094
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-07-05 01:38:32 +0200

    ..

--- bench_kmeans_wm.py
+++ bench_kmeans_wm.py
@@ -4,10 +4,15 @@ import sklearn
 from sklearn.cluster import KMeans
 import time
 
-n_iter = 10
+n_iter = 20
+
+n_sample = 100000
+n_feature= 50
+n_component = 1000
 
 working_memory = [1, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 250, 500, 1000, 2500, 5000, 10000]
-points = np.random.random_sample((100000, 50)).astype('float32')
+points = np.random.random_sample((n_sample, n_feature)).astype('float32')
+clusters = points[np.random.choice(n_sample, n_component, replace=False)]
 with open('bench_kmeans_wm.csv','w') as file:
     file.write("wm,time\n")
 
@@ -17,7 +22,7 @@ with open('bench_kmeans_wm.csv','w') as file:
         sklearn.set_config(working_memory=wm)
     
         t = time.time()
-        km = KMeans(init='random',
+        km = KMeans(init=clusters,
                     n_init=1,
                     tol=1.0e-16,
                     n_clusters=1000,
