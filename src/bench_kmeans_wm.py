eafa3895fefa66ef01c7d1d36b98f8899f718da2
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-07-13 11:41:46 +0200

    ..

--- src/bench_kmeans_wm.py
+++ src/bench_kmeans_wm.py
@@ -3,7 +3,7 @@ import sklearn
 from sklearn.cluster import KMeans
 import time
 
-n_iter = 20
+n_iter = 4
 
 n_sample = 100000
 n_feature = 50
@@ -11,9 +11,9 @@ n_component = 1000
 
 working_memory = [0.5, 1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
                   22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
-                  50, 60, 70, 80, 90, 100,
-                  200, 300, 400, 500, 600, 700, 800, 900, 1000,
-                  2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+                  45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+                  200, 400, 600, 800, 1000,
+                  2000, 4000, 6000, 8000, 10000]
 
 points = np.random.random_sample((n_sample, n_feature)).astype(np.float32)
 clusters = points[np.random.choice(n_sample, n_component, replace=False)]
@@ -21,23 +21,23 @@ clusters = points[np.random.choice(n_sample, n_component, replace=False)]
 with open('bench_kmeans_wm.csv', 'w') as file:
     file.write("wm,time\n")
 
-    for i, wm in enumerate(working_memory):
-        print(i / len(working_memory) * 100, "%")
+    for exp in range(10):
+        for i, wm in enumerate(working_memory):
+            print((exp*len(working_memory) + i) / (10*len(working_memory)) * 100, "%")
 
-        sklearn.set_config(working_memory=wm)
+            sklearn.set_config(working_memory=wm)
 
-        t = time.time()
-        km = KMeans(init=clusters,
-                    n_init=1,
-                    tol=1.0e-16,
-                    n_clusters=n_component,
-                    random_state=0,
-                    max_iter=n_iter,
-                    algorithm='full',
-                    precompute_distances=True)
-        km.fit(points)
-        print(km.inertia_)
-        t = time.time() - t
+            t = time.time()
+            km = KMeans(init=clusters,
+                        n_init=1,
+                        tol=0,
+                        n_clusters=n_component,
+                        random_state=0,
+                        max_iter=n_iter,
+                        algorithm='full',
+                        precompute_distances=True)
+            km.fit(points)
+            t = time.time() - t
 
-        s = str(wm) + "," + str(t) + "\n"
-        file.write(s)
+            s = str(wm) + "," + str(t) + "\n"
+            file.write(s)
