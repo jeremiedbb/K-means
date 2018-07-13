5b4fe3209feded22f933ba966462a5adfb29e42a
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-07-13 10:19:33 +0200

    ..

--- bin/bench_kmeans_wm.sh
+++ bin/bench_kmeans_wm.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source deactivate
 
-export MKL_NUM_THREADS=4
-export MKL_ENABLE_INSTRUCTIONS=AVX2
-
 rm ../bench/bench_kmeans_wm.csv
 
 export PYTHONPATH=${HOME}/scikit-learn:${PYTHONPATH}
--- src/bench_kmeans_wm.py
+++ src/bench_kmeans_wm.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 import sklearn
 from sklearn.cluster import KMeans
@@ -7,22 +6,26 @@ import time
 n_iter = 20
 
 n_sample = 100000
-n_feature= 50
+n_feature = 50
 n_component = 1000
 
-working_memory = [1, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 250, 500, 1000, 2500, 5000, 10000]
+working_memory = [0.5, 1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
+                  22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
+                  50, 60, 70, 80, 90, 100,
+                  200, 300, 400, 500, 600, 700, 800, 900, 1000,
+                  2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
 
 points = np.random.random_sample((n_sample, n_feature)).astype('float32')
 clusters = points[np.random.choice(n_sample, n_component, replace=False)]
 
-with open('../bench/bench_kmeans_wm.csv','w') as file:
+with open('../bench/bench_kmeans_wm.csv', 'w') as file:
     file.write("wm,time\n")
 
     for i, wm in enumerate(working_memory):
         print(i / len(working_memory) * 100, "%")
 
         sklearn.set_config(working_memory=wm)
-    
+
         t = time.time()
         km = KMeans(init=clusters,
                     n_init=1,
