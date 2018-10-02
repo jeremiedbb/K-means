ef99a2a836f4610a7d7fc4095b7bfbb92ccb0f98
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-10-02 17:45:57 +0200

    bench sparse

--- src/bench_kmeans_sparse.py
+++ src/bench_kmeans_sparse.py
@@ -45,3 +45,36 @@ def bench(dtype, density, n_tests, impl):
 
 
 bench(np.float32, 0.01, 1, 'PR')
+
+
+densities = [0.005, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2]
+
+
+def bench_density(n_samples, n_clusters, n_features, n_tests, impl):
+    n_tot = len(densities)
+    ts = np.zeros((n_tests, n_tot))
+
+    for test in range(n_tests):
+        for idx, d in enumerate(densities):
+            X = random(n_samples, n_features, density=d,
+                       format='csr', dtype=np.float32)
+
+            km = KMeans(n_clusters=n_clusters, init='random',
+                        n_init=1, max_iter=20, algorithm='full')
+
+            t = time.time()
+            km.fit(X)
+            t = time.time() - t
+            t /= km.n_iter_
+
+            ts[test, idx] = t
+
+    tms = ts.mean(axis=0)
+    tstds = ts.std(axis=0)
+
+    for idx, d in enumerate(densities):
+        print(impl + ',' + str(d) + ',' + str(tms[idx]) + ',' + str(tstds[idx]))
+
+
+# bench_density(2**16, 2**6, 2**10, 5, 'intel')
+# bench_density(2**10, 2**4, 2**2, 1, 'PR')
